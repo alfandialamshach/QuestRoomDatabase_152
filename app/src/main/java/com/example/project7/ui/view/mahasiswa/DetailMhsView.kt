55d7b6c43bc055b28546d1a500706f842885e975
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-13 14:35:30 +0700

    Menambakan fungsi privat pada file DetailMhsView.kt bernama DeleteConfirmationDialog

--- app/src/main/java/com/example/project7/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/project7/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,2 +1,30 @@
 package com.example.project7.ui.view.mahasiswa
 
+import android.app.AlertDialog
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.room.Delete
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCacel: () -> Unit, modifier: Modifier =
+        Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do Nothing */},
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCacel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
