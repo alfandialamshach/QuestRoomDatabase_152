aa2f9edc067285278569526868356a4b3fa06c5c
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-13 08:50:49 +0700

    Menambahkan kode di file LocalRepositoryMhs untuk menghubungkan MahasiswaDao

--- app/src/main/java/com/example/project7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/project7/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.project7.repository
 
 import com.example.project7.data.dao.MahasiswaDao
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,24 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    //getAllMhs
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+       return mahasiswaDao.getAllMahasiswa()
+    }
+
+    //getMhs
+    override fun getMhs(nim: String): Flow<Mahasiswa>{
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    //deleteMhs
+   override suspend fun deleteMhs(mahasiswa: Mahasiswa){
+       mahasiswaDao.deleteMahasiswa(mahasiswa)
+   }
+
+    //updateMhs
+   override suspend fun updateMhs(mahasiswa: Mahasiswa){
+       mahasiswaDao.updateMahasiswa(mahasiswa)
+   }
+
 }
