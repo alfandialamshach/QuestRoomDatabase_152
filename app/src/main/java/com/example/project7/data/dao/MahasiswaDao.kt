cc01bcafa35b0d03da888c4cb981223e7384cbf4
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-13 08:28:54 +0700

    Menambahkan kode di file MahasiswaDao untuk mendukung operasi operasi

--- app/src/main/java/com/example/project7/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/project7/data/dao/MahasiswaDao.kt
@@ -1,11 +1,29 @@
 package com.example.project7.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun  insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    //getMahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa (nim: String) : Flow<Mahasiswa>
+
+    //deleteMahasiswa
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    //UpdateMahasiswa
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
