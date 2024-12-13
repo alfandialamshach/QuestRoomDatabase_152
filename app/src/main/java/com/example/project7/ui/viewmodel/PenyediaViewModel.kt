c31adc7acb9dd7d69ecd9870bbfcae1e456a2e5f
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-13 10:57:52 +0700

    menambahkan kode initializer dalam file PenyediaViewModel.kt untuk mendefinisikan viewmodel diinisialisasi dengan dependensinya

--- app/src/main/java/com/example/project7/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project7/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.project7.ui.viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,6 +14,26 @@ object PenyediaViewModel{
         initializer {
             MahasiswaViewModel(
                 KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+            createSavedStateHandle(),
+            KrsApp().containerApp.repositoryMhs
             )
         }
     }
