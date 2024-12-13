b0ddf55d8eeee5dd2b78b771393ce1e31078dce2
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-13 08:32:31 +0700

    Menambahkan kode di file RepositoryMhs untuk mengelola data mahasiswa

--- app/src/main/java/com/example/project7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/project7/repository/RepositoryMhs.kt
@@ -1,8 +1,21 @@
 package com.example.project7.repository
 
 import com.example.project7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //getAllMhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    //getMhs
+    fun getMhs(nim: String) : Flow<Mahasiswa>
+
+    //deleteMhs
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    //updateMhs
+    suspend fun updateMhs (mahasiswa: Mahasiswa)
 }
