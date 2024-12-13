422579fbc5b1e0817b80ea81fc7a8955c39722a3
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-13 09:04:14 +0700

    Menambahkan kode di file Alamatnavigasi berupa objek-objek untuk tujuan navigasi

--- app/src/main/java/com/example/project7/ui/navigation/Alamatnavigasi.kt
+++ app/src/main/java/com/example/project7/ui/navigation/Alamatnavigasi.kt
@@ -3,3 +3,19 @@ package com.example.project7.ui.navigation
 interface Alamatnavigasi {
     val route : String
 }
+
+object DestinasiHome : Alamatnavigasi {
+    override val route = "home" //untuk mendefinisikan route sebagai home
+}
+
+object DestinasiDetail : Alamatnavigasi {
+    override  val route = "detail" //untuk mendefinisikan route sebagai detail
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : Alamatnavigasi {
+    override val route = "update" //untuk mendefinisikan route sebagai update
+    const val NIM = "nim"
+    val routeWithArg = "$route/{NIM}"
+}
